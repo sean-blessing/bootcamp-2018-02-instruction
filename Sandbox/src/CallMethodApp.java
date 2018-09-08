530653d3473699b5d972eef1cc26e796c34e4bf6
Sean Blessing <snblessing@gmail.com>
2018-09-08 16:37:59 -0400

    Bootcamp day 9 instruction

---
+++ Sandbox/src/CallMethodApp.java
@@ -0,0 +1,14 @@
+
+public class CallMethodApp {
+
+	public static void main(String[] args) {
+		printWelcomeMessage();
+
+		System.out.println("bye");
+	}
+	
+	private static void printWelcomeMessage() {
+		System.out.println("Welcome to the CallMethodApp!");
+	}
+
+}
