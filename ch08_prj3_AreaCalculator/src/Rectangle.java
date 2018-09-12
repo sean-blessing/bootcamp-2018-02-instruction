ba8397952bb44f25e812678ca1738390febe5ed0
Sean Blessing <snblessing@gmail.com>
2018-09-12 14:06:09 -0400

    chapter 8 area calculator app

---
+++ ch08_prj3_AreaCalculator/src/Rectangle.java
@@ -0,0 +1,24 @@
+
+public class Rectangle extends Square {
+
+	private double height;
+	
+	public Rectangle(double width, double height) {
+		super(width);
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double getArea() {
+		return width*height;
+	}
+
+}
