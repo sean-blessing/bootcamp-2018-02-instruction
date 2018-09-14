5c8a94c491317826be680850e5c63461ea10c453
Sean Blessing <snblessing@gmail.com>
2018-09-14 09:10:12 -0400

    updated show selector with arrays

--- ShowSelector_ClassDemo/src/ShowDB.java
+++ ShowSelector_ClassDemo/src/ShowDB.java
@@ -3,30 +3,75 @@ public class ShowDB {
 	
 	// This method will get an instance of Show for the
 	// searchCriteria and searchValue entered by the user
-	public static Show getShow(String sCriteria, String sValue) {
-		Show s = null;
+	public static Show[] getShow(String sCriteria, String sValue) {
+		Show[] shows = null;
 		
 		if (sCriteria.equalsIgnoreCase("g")) { // genre
 			if (sValue.equalsIgnoreCase("comedy")) {
-				s = new Show("Seinfeld", "comedy", 30, "Netflix");
+				shows = new Show[1];
+				Show s = new Show("Seinfeld", "comedy", 30, "Netflix");
+				shows[0] = s;
+				//shows = {s};  -> why doesn't this work??
 			}
 			else if (sValue.equalsIgnoreCase("drama")) {
-				s = new Show("West Wing", "drama", 60, "ABC");
+				shows = new Show[1];
+				Show s = new Show("West Wing", "drama", 60, "ABC");
+				shows[0] = s;
 			}
 			else if (sValue.equalsIgnoreCase("sci-fi")) {
-				s = new Show("Star Trek", "sci-fi", 60, "Hulu");
+				shows = new Show[1];
+				Show s = new Show("Star Trek", "sci-fi", 60, "Hulu");
+				shows[0] = s;
 			}			
 			else if (sValue.equalsIgnoreCase("reality")) {
-				s = new Show("Survivor", "reality", 90, "CBS");				
+				shows = new Show[1];
+				Show s = new Show("Survivor", "reality", 90, "CBS");				
+				shows[0] = s;
 			}			
 		}
 		else if (sCriteria.equalsIgnoreCase("l")) { // length
-			
+			if (sValue.equalsIgnoreCase("30")) {
+				shows = new Show[1];
+				Show s = new Show("Seinfeld", "comedy", 30, "Netflix");
+				shows[0] = s;
+			}
+			else if (sValue.equalsIgnoreCase("60")) {
+				shows = new Show[2];
+				Show s1 = new Show("West Wing", "drama", 60, "ABC");
+				Show s2 = new Show("Star Trek", "sci-fi", 60, "Hulu");
+				shows[0] = s1;
+				shows[1] = s2;
+			}
+			else if (sValue.equalsIgnoreCase("90")) {
+				shows = new Show[1];
+				Show s = new Show("Survivor", "reality", 90, "CBS");				
+				shows[0] = s;
+			}			
 		}
 		else if (sCriteria.equalsIgnoreCase("n")) { // network
-			
+			if (sValue.equalsIgnoreCase("Netflix")) {
+				shows = new Show[1];
+				Show s = new Show("Seinfeld", "comedy", 30, "Netflix");
+				shows[0] = s;
+				//shows = {s};  -> why doesn't this work??
+			}
+			else if (sValue.equalsIgnoreCase("ABC")) {
+				shows = new Show[1];
+				Show s = new Show("West Wing", "drama", 60, "ABC");
+				shows[0] = s;
+			}
+			else if (sValue.equalsIgnoreCase("Hulu")) {
+				shows = new Show[1];
+				Show s = new Show("Star Trek", "sci-fi", 60, "Hulu");
+				shows[0] = s;
+			}			
+			else if (sValue.equalsIgnoreCase("CBS")) {
+				shows = new Show[1];
+				Show s = new Show("Survivor", "reality", 90, "CBS");				
+				shows[0] = s;
+			}			
 		}
-		return s;
+		return shows;
 	}
 
 }
