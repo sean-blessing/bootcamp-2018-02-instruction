e0ddc164016dc834790c21870aecb79948520476
Sean Blessing <snblessing@gmail.com>
2018-09-13 20:42:00 -0400

    Array class demo

---
+++ Sandbox/src/ArrayDemoApp.java
@@ -0,0 +1,22 @@
+
+public class ArrayDemoApp {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+		double[] prices = {14.95, 44.98, 57.22, 79.11};
+		double sum = 0;
+//		for (int i = 0; i < prices.length; i++) {
+//			System.out.println(prices[i]);
+//			sum += prices[i];
+//			System.out.println("Subtotal = "+sum);
+//		}
+		for (double price : prices) {
+			System.out.println(price);
+			sum += price;
+			System.out.println("Subtotal = "+sum);
+		}
+		System.out.println("bye");
+	}
+
+}
