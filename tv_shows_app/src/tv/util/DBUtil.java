23f566d5ed441e042b29d9b7d591d2f53a9f11d8
Sean Blessing <snblessing@gmail.com>
2018-09-21 06:12:13 -0400

    cleaned up DBUtil.java

--- tv_shows_app/src/tv/util/DBUtil.java
+++ tv_shows_app/src/tv/util/DBUtil.java
@@ -13,29 +13,18 @@ public class DBUtil {
     private DBUtil() {}
 
     public static Connection getConnection() throws SQLException {
-        try {
-            // set the db url, username, and password
-            String url = "jdbc:mysql://localhost:3306/tv_shows";
-            String username = "tv_shows_user";
-            String password = "sesame";
+        // define connection parameters    
+    	String url = "jdbc:mysql://localhost:3306/tv_shows";
+        String username = "tv_shows_user";
+        String password = "sesame";
 
-            // get and return connection
-            Class.forName("com.mysql.jdbc.Driver").newInstance(); 
-            connection = DriverManager.getConnection(
-                    url, username, password);
-            //return connection;
-        } catch (SQLException e) {
-        	System.out.println("Error establishing connection!");
-            throw e;
-        }  
-        catch (IllegalAccessException|InstantiationException|ClassNotFoundException me) {
-        	System.err.println("exception in DBUtil...");
-        	me.printStackTrace();
-        }
+        //get and return connection
+        connection = DriverManager.getConnection(url, username, password);
         return connection;
-        
     }
     
+    // Shouldn't need to call this if getConnection() called 
+    // within try-with-resources statement
     public static void closeConnection()  throws SQLException{
         if (connection != null) {
             try {
